0f94d5dad0d87493f1bf2988129145b92b086518
Nattan <nattan078@gmail.com>
2023-02-15 22:51:09 -0300

    Iniciando projeto
    Aqui foi feita uma tela de Login simples

---
+++ ProjetoTeste/src/main/java/com/main/Main.java
@@ -0,0 +1,11 @@
+package com.main;
+
+import com.telas.TelaLogin;
+
+public class Main {
+
+    public static void main(String[] args) {   
+        TelaLogin tela = new TelaLogin();
+        tela.setVisible(true);
+    }
+}
---
+++ ProjetoTeste/src/main/java/com/telas/TelaLogin.java
@@ -0,0 +1,269 @@
+package com.telas;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class TelaLogin extends javax.swing.JFrame {
+    
+    int positionX = 0, positionY = 0;
+    
+    public TelaLogin() {
+        initComponents();
+        scaleImage();
+    }
+    
+    public void scaleImage() {
+        ImageIcon icon = new ImageIcon("C:\\Users\\natta\\Documents\\NetBeansProjects\\ProjetoTeste\\src\\main\\java\\com\\telas\\icons\\logo.png");
+        Image img = icon.getImage();
+        Image imgScale = img.getScaledInstance(lblLogo.getWidth(), lblLogo.getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(imgScale);
+        lblLogo.setIcon(scaledIcon);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bgDados = new javax.swing.JPanel();
+        txtLogin = new javax.swing.JTextField();
+        passSenha = new javax.swing.JPasswordField();
+        btnEntrar = new javax.swing.JButton();
+        lblBemVindo = new javax.swing.JLabel();
+        lblLogin = new javax.swing.JLabel();
+        lblSenha = new javax.swing.JLabel();
+        btnSair = new javax.swing.JButton();
+        btnMinimizar = new javax.swing.JButton();
+        bgLogo = new javax.swing.JPanel();
+        lpLogo = new javax.swing.JLayeredPane();
+        lblLogo = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("E-SELL");
+        setUndecorated(true);
+        setResizable(false);
+        addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                formMouseDragged(evt);
+            }
+        });
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                formMousePressed(evt);
+            }
+        });
+
+        bgDados.setBackground(new java.awt.Color(90, 228, 168));
+
+        txtLogin.setBackground(new java.awt.Color(255, 255, 255));
+        txtLogin.setFont(new java.awt.Font("Roboto Lt", 0, 16)); // NOI18N
+        txtLogin.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        txtLogin.setAutoscrolls(false);
+        txtLogin.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 10, 1, 10));
+        txtLogin.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
+        txtLogin.setMargin(new java.awt.Insets(30, 30, 30, 30));
+
+        passSenha.setBackground(new java.awt.Color(255, 255, 255));
+        passSenha.setFont(new java.awt.Font("Roboto Lt", 0, 16)); // NOI18N
+        passSenha.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        passSenha.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 10, 1, 10));
+
+        btnEntrar.setBackground(new java.awt.Color(0, 0, 0));
+        btnEntrar.setFont(new java.awt.Font("Roboto Th", 1, 22)); // NOI18N
+        btnEntrar.setForeground(new java.awt.Color(255, 255, 255));
+        btnEntrar.setText("Entrar");
+        btnEntrar.setBorder(null);
+        btnEntrar.setBorderPainted(false);
+        btnEntrar.setFocusCycleRoot(true);
+
+        lblBemVindo.setBackground(new java.awt.Color(116, 116, 116));
+        lblBemVindo.setFont(new java.awt.Font("Roboto", 0, 40)); // NOI18N
+        lblBemVindo.setForeground(new java.awt.Color(0, 0, 0));
+        lblBemVindo.setText("BEM VINDO");
+
+        lblLogin.setBackground(new java.awt.Color(0, 0, 0));
+        lblLogin.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+        lblLogin.setForeground(new java.awt.Color(0, 0, 0));
+        lblLogin.setText("Login");
+
+        lblSenha.setBackground(new java.awt.Color(0, 0, 0));
+        lblSenha.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+        lblSenha.setForeground(new java.awt.Color(0, 0, 0));
+        lblSenha.setText("Senha");
+
+        btnSair.setBackground(new java.awt.Color(90, 228, 168));
+        btnSair.setFont(new java.awt.Font("Verdana", 1, 20)); // NOI18N
+        btnSair.setForeground(new java.awt.Color(0, 0, 0));
+        btnSair.setText("X");
+        btnSair.setBorder(null);
+        btnSair.setBorderPainted(false);
+        btnSair.setFocusPainted(false);
+        btnSair.setOpaque(true);
+        btnSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSairActionPerformed(evt);
+            }
+        });
+
+        btnMinimizar.setBackground(new java.awt.Color(90, 228, 168));
+        btnMinimizar.setFont(new java.awt.Font("Verdana", 1, 20)); // NOI18N
+        btnMinimizar.setForeground(new java.awt.Color(0, 0, 0));
+        btnMinimizar.setText("-");
+        btnMinimizar.setBorder(null);
+        btnMinimizar.setBorderPainted(false);
+        btnMinimizar.setFocusPainted(false);
+        btnMinimizar.setOpaque(true);
+        btnMinimizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMinimizarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout bgDadosLayout = new javax.swing.GroupLayout(bgDados);
+        bgDados.setLayout(bgDadosLayout);
+        bgDadosLayout.setHorizontalGroup(
+            bgDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bgDadosLayout.createSequentialGroup()
+                .addContainerGap(123, Short.MAX_VALUE)
+                .addGroup(bgDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgDadosLayout.createSequentialGroup()
+                        .addComponent(btnMinimizar, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnSair, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgDadosLayout.createSequentialGroup()
+                        .addGroup(bgDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(lblLogin)
+                                .addGroup(bgDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(btnEntrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(txtLogin, javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(passSenha, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 276, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(lblSenha))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgDadosLayout.createSequentialGroup()
+                                .addComponent(lblBemVindo)
+                                .addGap(37, 37, 37)))
+                        .addGap(110, 110, 110))))
+        );
+        bgDadosLayout.setVerticalGroup(
+            bgDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bgDadosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(bgDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSair, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnMinimizar, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(71, 71, 71)
+                .addComponent(lblBemVindo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblLogin)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblSenha)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(passSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(24, 24, 24)
+                .addComponent(btnEntrar, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        bgLogo.setBackground(new java.awt.Color(255, 255, 255));
+
+        lpLogo.setLayer(lblLogo, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout lpLogoLayout = new javax.swing.GroupLayout(lpLogo);
+        lpLogo.setLayout(lpLogoLayout);
+        lpLogoLayout.setHorizontalGroup(
+            lpLogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(lpLogoLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblLogo, javax.swing.GroupLayout.DEFAULT_SIZE, 261, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        lpLogoLayout.setVerticalGroup(
+            lpLogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(lpLogoLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblLogo, javax.swing.GroupLayout.DEFAULT_SIZE, 111, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout bgLogoLayout = new javax.swing.GroupLayout(bgLogo);
+        bgLogo.setLayout(bgLogoLayout);
+        bgLogoLayout.setHorizontalGroup(
+            bgLogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bgLogoLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lpLogo)
+                .addContainerGap())
+        );
+        bgLogoLayout.setVerticalGroup(
+            bgLogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bgLogoLayout.createSequentialGroup()
+                .addGap(174, 174, 174)
+                .addComponent(lpLogo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(209, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(bgLogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(0, 0, 0)
+                .addComponent(bgDados, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bgDados, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(bgLogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSairActionPerformed
+        // TODO add your handling code here:
+        System.exit(1);
+    }//GEN-LAST:event_btnSairActionPerformed
+
+    private void formMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMousePressed
+        // TODO add your handling code here:
+        positionX = evt.getX();
+        positionY = evt.getY();
+    }//GEN-LAST:event_formMousePressed
+
+    private void formMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseDragged
+        // TODO add your handling code here:
+        setLocation(evt.getXOnScreen() - positionX, evt.getYOnScreen() - positionY);
+    }//GEN-LAST:event_formMouseDragged
+
+    private void btnMinimizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMinimizarActionPerformed
+        // TODO add your handling code here:
+        this.setExtendedState(TelaLogin.ICONIFIED);
+    }//GEN-LAST:event_btnMinimizarActionPerformed
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaLogin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel bgDados;
+    private javax.swing.JPanel bgLogo;
+    private javax.swing.JButton btnEntrar;
+    private javax.swing.JButton btnMinimizar;
+    private javax.swing.JButton btnSair;
+    private javax.swing.JLabel lblBemVindo;
+    private javax.swing.JLabel lblLogin;
+    private javax.swing.JLabel lblLogo;
+    private javax.swing.JLabel lblSenha;
+    private javax.swing.JLayeredPane lpLogo;
+    private javax.swing.JPasswordField passSenha;
+    private javax.swing.JTextField txtLogin;
+    // End of variables declaration//GEN-END:variables
+}
